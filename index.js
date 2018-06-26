7a7a6b0258dc2a38d9dfcf2fdb5c467f994dc34c
kalex19 <klewis@rockymountainprep.org>
2018-06-26 04:05:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function happyHolidays(string){return ("Happy holidays!") }
 function happyHolidaysTo (name) {return (`Happy holidays, ${name}!`) }
 function happyHolidayTo(holiday, name) { return (`Happy ${holiday}, ${name}!`) }
 function holidayCountdown(holiday, day) 
-console.log (days = '15') 
-{return (`It's ${days} days until ${holiday}!`) }
+console.log (day = '15') 
+{return (`It's ${day} days until ${holiday}!`) }
